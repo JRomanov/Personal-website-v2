b6c77f7d6ed6903f7e4d06dff747ca09c3790266
James Romanov <jromanov@sdsu.edu>
2023-05-01 18:58:55 -0700

    Progess on nav bar for desktop and mobile.

---
+++ app.js
@@ -0,0 +1,7 @@
+// for mobile burger menu
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector("#navLinks")
+
+burgerIcon.addEventListener("click", () => {
+    navbarMenu.classList.toggle("is-active")
+});
