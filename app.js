1abf4f0d459f3a1ae8a0e2c1af8ff429de593f3a
James Romanov <jromanov@sdsu.edu>
2023-05-09 23:24:10 -0700

    Progression on webpage.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // for mobile burger menu
+document.addEventListener('DOMContentLoaded', function () {
+
+AOS.init();
+
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#navLinks");
 
 burgerIcon.addEventListener("click", () => {
     navbarMenu.classList.toggle("is-active")
+});
+
 });
