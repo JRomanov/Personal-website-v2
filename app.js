7920904c4c3704c20a6b68ddcee0a01cac087ad2
James <jromanov@sdsu.edu>
2023-06-07 17:35:42 -0700

    Jump to element from nav bar. Smooth scroll will work on later.

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // for mobile burger menu
 document.addEventListener('DOMContentLoaded', function () {
 
-AOS.init();
-
 const burgerIcon = document.querySelector("#burger");
-const navbarMenu = document.querySelector("#navLinks");
+//const navbarMenu = document.querySelector("#navLinks");
+const aboutMe = document.querySelector("#About");
+const aboutMeNav = document.querySelector("#aboutMeNavbar")
 
 burgerIcon.addEventListener("click", () => {
-    navbarMenu.classList.toggle("is-active")
+    burgerIcon.classList.toggle("is-active")
 });
+
+/* aboutMeNav.addEventListener("click", () => {
+    aboutMeNav.scrollIntoView(aboutMe, {behavior:"smooth"})
+}); */
 
 });
